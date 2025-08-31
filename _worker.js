9fc6238edc94ce5ae0af8aafbcc044f527cb04d9
xuwu85 <76637922+xuwu85@users.noreply.github.com>
2025-08-31 17:46:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '60c125db-bbb8-4e45-887c-47aed0376664';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
